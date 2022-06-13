28221838667f48a79e7074619dc9cb12c80530cf
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-06-13 03:12:35 -0300

    chore: add styled-components and configure react-router

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import Home from './pages/Home/index'
 import Display from './pages/DisplayTerminal/index'
 import Password from './pages/PasswordTerminal/index'
 import Service from './pages/ServiceTerminal/index'
+import GlobalStyles from './styles/global'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/home' element={<Home />} />
-      <Route path='/password' element={<Password />} />
-      <Route path='/service' element={<Service />} />
-      <Route path='/display' element={<Display />} />
-    </Routes>
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/password' element={<Password />} />
+        <Route path='/service' element={<Service />} />
+        <Route path='/display' element={<Display />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    // Colors
+    --white: #ffff;
+    --black: #000000;
+    --red-maroon: #3D0000;
+    --red-strong: #950101;
+    --red-high: #FF0000;
+    // Spaces
+    --space-lang: 1400px;
+    --space-short: 700px;
+    // Fonts
+    --font-title: 4em;
+    --font-medium: 1.5em;
+    --font-small: 'PT Sans', sans-serif;
+  }
+  
+  body{
+    background-color: var(--black);
+    color: var(--white);
+    font-family: var(--font-small);
+    font-size: 0.95em;
+  }
+  
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default GlobalStyles
