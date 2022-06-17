3354305a8fd5b664c9b46a6e1d056af255841e87
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-06-17 19:49:42 -0300

    fix: remove api consume

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const apiPasswords = () => {
-  const response = axios.get('http://localhost:8877/passwords')
-
-  return response
-}
-
-module.export = { apiPasswords }
